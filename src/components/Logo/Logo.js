5f256c54d576eca909a50ae48be1e6ac59b406e2
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-15 19:32:55 -0800

    built navigation template and logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+  return(
+    <div className='d-flex justify-content-center pt-4'>
+      <br/>
+      <Tilt className="Tilt" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+        <div className="Tilt-inner">
+          <img className='pt-3 pl-3' style={{width: '80px'}} alt='logo' src={brain}/>
+        </div>
+      </Tilt>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+  return(
+    <nav className='navbar navbar-dark bg-dark'>
+      <a className="navbar-brand" href="#">Face Recognition</a>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#">Sign Out</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
