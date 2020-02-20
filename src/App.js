1c531143722d05a651e08f0f53e810542c855e59
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-19 22:14:13 -0800

    added register functionality

--- src/App.js
+++ src/App.js
@@ -22,8 +22,27 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   componentDidMount() {
@@ -93,7 +112,7 @@ class App extends Component {
         : (
           route === 'signin'
           ? <Signin onRouteChange={ this.onRouteChange } />
-          : <Register onRouteChange={ this.onRouteChange } />
+          : <Register loadUser={this.loadUser} onRouteChange={ this.onRouteChange } />
           )
         }
       </div>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Register = ({ onRouteChange }) => {
-  return(
-    <div className='card col-md-3 p-4 mx-auto mt-4' style={{backgroundColor: '#f8f9fa'}}>
-      <div>
-        <h3>Register</h3>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" placeholder="Enter name" />
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  onNameChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitRegister = () => {
+    fetch('http://localhost:3001/register', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if(user) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
+  }
+
+  render() {
+    return(
+      <div className='card col-md-3 p-4 mx-auto mt-4' style={{backgroundColor: '#f8f9fa'}}>
+        <div>
+          <h3>Register</h3>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="name" 
+              placeholder="Enter name" 
+              onChange={this.onNameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input 
+              type="email" 
+              className="form-control" 
+              id="email" 
+              placeholder="Enter email" 
+              onChange={this.onEmailChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password" 
+              className="form-control" 
+              id="password" 
+              placeholder="Password" 
+              onChange={this.onPasswordChange}
+            />
+          </div>
+          <button onClick={this.onSubmitRegister} type="submit" className="btn btn-primary">Register</button>
+          <small onClick={() => this.props.onRouteChange('signin')} className='btn'>Signin</small>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input type="email" className="form-control" id="email" placeholder="Enter email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" className="form-control" id="password" placeholder="Password" />
-        </div>
-        <button onClick={() => onRouteChange('home')} type="submit" className="btn btn-primary">Register</button>
-        <small onClick={() => onRouteChange('signin')} className='btn'>Signin</small>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Register;
