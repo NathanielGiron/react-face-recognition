d03b576609ea9182c6905a0f024796a5cff3e63b
Nathaniel Giron <nathaniel.giron@gmail.com>
2020-02-19 22:57:41 -0800

    updating entries count

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return(
     <div className='pt-4'>
       <p>{'This will detect faces in your pictures. Give it a try.'}</p>
 
       <div className='text-center'>
         <input className='form-control' onChange={onInputChange} />
-        <button className='btn btn-primary' onClick={onButtonSubmit}>Detect</button>
+        <button className='btn btn-primary' onClick={onPictureSubmit}>Detect</button>
       </div>
     </div>
   );
